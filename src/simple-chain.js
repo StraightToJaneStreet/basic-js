4d78499aa4c1b4674379dbbc248bfd9b5b73b724
Straight To <straighttojanestreet@rs.school>
2022-04-04 14:19:36 +0300

    init: add simple-chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,31 +1,113 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Implement chainMaker object according to task description
- * 
- */
+function Node(value) {
+  return {
+    value, next: null, prev: null
+  }
+}
+
 const chainMaker = {
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let cursor = this.root
+    let len = 0
+    while (cursor != null) {
+      len++
+      cursor = cursor.next
+    }
+    return len
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+
+    if (this.root == null) {
+      this.root = Node(value)
+      return this
+    }
+
+    let cursor = this.root
+    let prevNode
+
+    while (cursor != null) {
+      [prevNode, cursor] = [cursor, cursor.next]
+    }
+
+    prevNode.next = Node(value)
+    prevNode.next.prev = prevNode
+
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+
+    if (!(typeof position == 'number') || position < 1 || position > this.getLength()) {
+      this.root = null
+      throw Error('You can\'t remove incorrect link!')
+    }
+
+    position--
+    let cursor = this.root
+    while (position > 0 && cursor != null) {
+      cursor = cursor.next
+      position--
+    }
+    if (this.root == cursor) {
+      this.root = cursor.next
+    }
+    if (cursor.prev != null) {
+      cursor.prev.next = cursor.next
+    }
+    if (cursor.next != null) {
+      cursor.next.prev = cursor.prev
+    }
+    return this
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.root == null) {
+      return this
+    }
+    let cursor = this.root
+    let prevNode = cursor
+    while (cursor != null) {
+      const {prev, next} = cursor
+      cursor.prev = next
+      cursor.next = prev
+      prevNode = cursor
+      cursor = next
+    }
+    this.root = prevNode
+    return this
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let parts = []
+    let cursor = this.root
+    while (cursor != null) {
+      parts.push(`( ${cursor.value} )`)
+      cursor = cursor.next
+    }
+    this.root = null
+    const result = parts.join('~~')
+    return result
   }
 };
+
+let res = chainMaker
+  .addLink(3.14)
+  .addLink(1)
+  .addLink({ 0: 'first', 1: 'second', 'length': 2 })
+  .removeLink(1)
+  .addLink('DEF')
+  .addLink({ 0: 'first', 1: 'second', 'length': 2 })
+  .removeLink(1)
+  .addLink(true)
+  .addLink(false)
+  .addLink(333)
+  // .reverseChain()
+  // .reverseChain()
+  .finishChain()
+console.log(res)
 
 module.exports = {
   chainMaker
