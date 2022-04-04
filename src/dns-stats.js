d2b3d683b1410da1d2a2d2ddc344f3b18f4611b1
Straight To <straighttojanestreet@rs.school>
2022-04-04 14:20:30 +0300

    init: add dns-stats solution

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+
+// Сейчас бы префиксное дерево написать
+function Tree(value = 0) {
+  this.count = value
+  this.edges = {}
+  this.hasEdge = (mark) => mark in this.edges
+  this.getChild = (mark) => this.edges[mark]
+  this.addEdge = (mark) => this.edges[mark] = new Tree()
+  this.getCount = () => this.count
+  return this
+}
+
+function collect(node, prefix = '') {
+  const base = prefix != '' ? { [prefix]: node.count } : {}
+  return Object.entries(node.edges)
+    .reduce((domains, [p, t]) => Object.assign(domains, collect(t, prefix + '.' + p)), base)
+}
+
+function getDNSStats(source) {
+  const root = new Tree()
+  for (let a of source) {
+    const path = a.split('.').reverse()
+    let cursor = root
+    for (let p of path) {
+      if (cursor.hasEdge(p)) cursor = cursor.getChild(p)
+      else cursor = cursor.addEdge(p)
+      cursor.count++
+    }
+  }
+  return collect(root)
 }
 
 module.exports = {
